5156ab60fccf7f72b875f807f01c9ec4096d2cbb
morachake <jacobmoracha@gmail.com>
2024-01-10 23:41:28 +0300

    adding screens for the sidebar

---
+++ src/views/home/HelpCenter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function HelpCenter() {
+  return (
+    <View>
+        <Text>Help</Text>
+    </View>
+  );
+}
---
+++ src/views/home/Safety.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Safety() {
+  return (
+    <View>
+        <Text>Safety</Text>
+    </View>
+  );
+}
