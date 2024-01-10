de33ecfee92005f300d5d41ad7ff96714766deee
morachake <jacobmoracha@gmail.com>
2024-01-11 00:17:38 +0300

    help center

--- src/views/home/HelpCenter.js
+++ src/views/home/HelpCenter.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-
+import Ionicons from '@expo/vector-icons/Ionicons';
 export default function HelpCenter() {
   return (
     <View>
         <Text>Help</Text>
+        <Ionicons name="md-checkmark-circle" size={32} color="green" />cv 
     </View>
   );
 }
