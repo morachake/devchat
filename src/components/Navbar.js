848b4c3fff7f613147c8e5b0959e114d61911d2d
morachake <jacobmoracha@gmail.com>
2024-01-15 23:47:56 +0300

    adding a navabr

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,25 @@
-import { Text, View } from "react-native";
-
-export default function Header () {
+import { StyleSheet, Text, View } from "react-native";
+import { MaterialIcons,FontAwesome5 } from '@expo/vector-icons';
+export default function NavBar () {
     return (
-        <View>
-            <Text>Add as story</Text>
+        <View style={styles.container}>
+            <>
+                <MaterialIcons name="message" size={40} color="#F64A69" />
+                <Text style={{color:"#FFFF",fontSize:15}}>Add new Message</Text>
+            </>
+           
+           <FontAwesome5 name="archive" size={24} color="#BADAFF" />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container :{
+        flexDirection: 'row',
+        alignContent: 'center',
+        justifyContent: 'space-between',
+        height: 50,
+        width: '100%',
+        paddingHorizontal: 18
+    }
+})
--- src/views/home/Chat.js
+++ src/views/home/Chat.js
@@ -2,10 +2,12 @@ import { View,Text,StyleSheet, StatusBar } from "react-native";
 import {COLORS} from "../../constants/index"
 import SearchBar from "../../components/SearchBar";
 import { SafeAreaView } from "react-native-safe-area-context";
+import NavBar from "../../components/Navbar";
 export default function Chat () {
     return(
-        <SafeAreaView>
-        <StatusBar style="auto"/>
+        <SafeAreaView style={styles.container}>
+            <StatusBar style="auto"/>
+            <NavBar/>
             <View style={styles.container}>
                 <SearchBar />
             </View>
