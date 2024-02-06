39b454b610589367832fd5b3bcd674a387418204
morachake <jacobmoracha@gmail.com>
2024-02-06 15:52:17 +0300

    Feat:adding a commit or stash swipe

--- src/components/FooterBtn.js
+++ src/components/FooterBtn.js
@@ -1,11 +1,28 @@
 import {TouchableWithoutFeedback} from "react-native-gesture-handler";
 import { FontAwesome } from '@expo/vector-icons';
-import {View} from "react-native";
+import {View,Animated} from "react-native";
+import {useCallback, useRef} from "react";
 
 export default  function ({name,size,color,style,onPress}){
+    const scale = useRef(new Animated.Value(1)).current;
+
+    const animatedScale = useCallback((newValue)=>{
+            Animated.spring(scale,{
+                toValue:newValue,
+                triction:4,
+                useNativeDriver: true
+            }).start()
+    },[scale])
     return(
-        <TouchableWithoutFeedback>
-            <View
+        <TouchableWithoutFeedback
+            onPressIn={()=>animatedScale(0.6)}
+            onPressOut={()=>{
+                animatedScale(1)
+            }}
+            delayPressIn={0}
+            delayPressOut={100}
+        >
+            <Animated.View
                 style={{
                     height:60,
                     width: 60,
@@ -15,7 +32,9 @@ export default  function ({name,size,color,style,onPress}){
                     justifyContent: 'center',
                     alignItems: 'center',
                     borderColor: color,
-                    borderWidth: 1.2
+                    borderWidth: 1.2,
+                    transform:[{scale}],
+                    ...style
                 }}
             >
                 <FontAwesome
@@ -23,7 +42,7 @@ export default  function ({name,size,color,style,onPress}){
                     size={size}
                     color={color}
                 />
-            </View>
+            </Animated.View>
         </TouchableWithoutFeedback>
     )
 }
